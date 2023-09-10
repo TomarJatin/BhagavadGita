d13ba909d93d5073777d4964303e34b2f3b16e5c
TomarJatin <jt24072002@gmail.com>
2023-09-11 01:16:07 +0530

    fonts and globalStyles added

--- App.js
+++ App.js
@@ -4,15 +4,24 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/Home';
+import {  useFonts, Inter_900Black } from '@expo-google-fonts/inter';
 
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ GlobalStyles.js
@@ -0,0 +1,26 @@
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+
+
+
+// const mode = AsyncStorage.getItem("mode");
+/* fonts */
+export const FontFamily = {
+  Inter: 'Inter'
+};
+/* font sizes */
+export const FontSize = {
+  rehular10pxRegular_size: RFValue(10),
+};
+
+/* Colors */
+export const color = (theme) => ({
+  white: theme === 'light' ? "#fff": '#181818',
+})
+/* Paddings */
+export const Padding = {
+  
+};
+/* border radiuses */
+export const Border = {
+ 
+};
