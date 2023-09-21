5d265093f049041fdb42f7a31420dad3022e3552
TomarJatin <jt24072002@gmail.com>
2023-09-21 08:49:29 +0530

    save for later context added

---
+++ contexts/SaveForLaterContext.js
@@ -0,0 +1,30 @@
+import React, { createContext, useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Toast from "react-native-simple-toast";
+// import FormData from "form-data";
+
+// Define the context
+const SaveForLaterContext = createContext();
+
+// Data Provider component
+const SaveForLaterProvider = ({ children }) => {
+  const [saveForLater, setSaveForLater] = useState([]);
+
+  useEffect(() => {
+    console.log("save for later context changed ========================");
+  });
+
+
+  return (
+    <SaveForLaterContext.Provider
+      value={{
+        saveForLater, setSaveForLater
+      }}
+    >
+      {children}
+    </SaveForLaterContext.Provider>
+  );
+};
+
+export { SaveForLaterContext, SaveForLaterProvider };
